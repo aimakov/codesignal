ee5101ccfa86df6873fcdea620726d1742a1a8e9
AIMAKOVNURBOLAT <aimakov@unist.ac.kr>
2020-07-12 17:26:13 +0900

    arrayChange solved time issue

--- arrayChange.py
+++ arrayChange.py
@@ -3,9 +3,10 @@
 def arrayChange(inputArray):
 	ans = 0
 	for i in range(1,len(inputArray)):
-		while inputArray[i]<=inputArray[i-1]:
-			inputArray[i]+=1
-			ans+=1
+		if inputArray[i]<=inputArray[i-1]:
+			ans+=inputArray[i-1]-inputArray[i]+1
+			inputArray[i]+=inputArray[i-1]-inputArray[i]+1
+			
 	return ans
 
 
