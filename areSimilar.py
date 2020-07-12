d99a4e717cd3db85b382048a7e20593e215a3aa5
AIMAKOVNURBOLAT <aimakov@unist.ac.kr>
2020-07-12 15:15:44 +0900

    areSimilar Solved

--- areSimilar.py
+++ areSimilar.py
@@ -1,2 +1,23 @@
-for i in range(3):
-    print(i)
+# Program to test if two arrays are similar - no more than 1 change
+
+def areSimilar(a,b):
+
+    a_sorted=sorted(a)
+    b_sorted=sorted(b)
+
+    if len(a_sorted)!=len(b_sorted):
+        return False
+    else:
+        for i in range(len(a_sorted)):
+            if a_sorted[i]!=b_sorted[i]:
+                return False
+    
+    temp = 0
+
+    for i in range(len(a)):
+    	if a[i]!=b[i]:
+    		temp+=1
+    	if temp >2:
+    		return False
+
+    return True
