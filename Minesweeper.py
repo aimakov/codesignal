c9ec729e3b1dfc6fbf077f53bafbe608d6d00820
AIMAKOVNURBOLAT <aimakov@unist.ac.kr>
2020-07-20 23:12:09 +0900

    minesweeper solved

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,26 +1,53 @@
-def test(matrix,x,y):
-	temp = 0
-	for i in range(x-1,x+2):
-		for j in range(y-1,y+2):
-			try:
-				if i>=0 and j>=0 and (i!=x or j!=y):
-					if matrix[i][j] == True:
-						temp+=1
-				# else:
-					# print("lol2")
-			except IndexError:
-				pass
-	return temp		
+# def test(matrix,x,y):
+# 	temp = 0
+# 	for i in range(x-1,x+2):
+# 		for j in range(y-1,y+2):
+# 			try:
+# 				if i>=0 and j>=0 and (i!=x or j!=y):
+# 					if matrix[i][j] == True:
+# 						temp+=1
+# 				# else:
+# 					# print("lol2")
+# 			except IndexError:
+# 				pass
+# 	return temp		
+
+# matrix = [[True, False, False],
+#           [False, True, False],
+#           [False, False, False]]
+
+# ans = [[0 for x in range(len(matrix[0]))] for x in range(len(matrix))] 
+
+# for x in range(len(matrix)):
+# 	for y in range(len(matrix[0])):
+# 		ans[x][y] = test(matrix,x,y)
+
+# print(ans)
+
+
+
+def test(matrix):
+
+	ans = [[0 for x in range(len(matrix[0]))] for x in range(len(matrix))]
+
+	for x in range(len(matrix)):
+		for y in range(len(matrix[0])):
+			temp = 0
+			for i in range(x-1,x+2):
+				for j in range(y-1,y+2):
+					try:
+						if i>=0 and j>=0 and (i!=x or j!=y):
+							if matrix[i][j] == True:
+								temp+=1
+						# else:
+							# print("lol2")
+					except IndexError:
+						pass
+			ans[x][y] = temp
+	return ans
 
 matrix = [[True, False, False],
           [False, True, False],
           [False, False, False]]
 
-ans = [[0 for x in range(len(matrix[0]))] for x in range(len(matrix))] 
-
-for x in range(len(matrix)):
-	for y in range(len(matrix[0])):
-		ans[x][y] = test(matrix,x,y)
-
-print(ans)
-
+print(test(matrix))
